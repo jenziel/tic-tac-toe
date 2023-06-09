4412cd8b5b3114c600cff28af620d3dbaad8c656
Jen Ziel <jenziel400@gmail.com>
2023-06-09 12:16:50 -0400

    Add event listeners to detect when the tic-tac-toe grid is clicked.  Update title prompt to display which user's turn it is.  Alternate back and forth between displaying player1 and player2's symbol upon clicking the grid cells.

--- main.js
+++ main.js
@@ -2,19 +2,95 @@
 var scoreboardLeft = document.querySelector("#left-side-wins")
 var scoreboardRight = document.querySelector("#right-side-wins")
 var promptTurn = document.querySelector("#prompt-user-turn")
-var gridBox1 = document.querySelector("#1")
-var gridBox2 = document.querySelector("#2")
-var gridBox3 = document.querySelector("#3")
-var gridBox4 = document.querySelector("#4")
-var gridBox5 = document.querySelector("#5")
-var gridBox6 = document.querySelector("#6")
-var gridBox7 = document.querySelector("#7")
-var gridBox8 = document.querySelector("#8")
-var gridBox9 = document.querySelector("#9")
+var displaySymbol = document.querySelector(".display-symbol")
+var buttonContainer = document.querySelectorAll(".grid-container")
+var cellButtons = document.querySelectorAll(".cell-button")
+var mainPane = document.querySelector("main")
+var button1 = document.querySelector("#btn1")
+var button2 = document.querySelector("#btn2")
+var button3 = document.querySelector("#btn3")
+var button4 = document.querySelector("#btn4")
+var button5 = document.querySelector("#btn5")
+var button6 = document.querySelector("#btn6")
+var button7 = document.querySelector("#btn7")
+var button8 = document.querySelector("#btn8")
+var button9 = document.querySelector("#btn9")
 
+var buttonsArray = [button1, button2, button3, button4, button5, button6, button7, button8, button9]
 // Event listeners:
-window.addEventListener("load", showBlankBoard)
+// window.addEventListener("load", showBlankBoard)
 
+mainPane.addEventListener("click", function(){
+    switchPlayer()
+    displayPlayerTurn()
+})
+
+button1.addEventListener("click", function(event){
+    if(promptTurn.classList.contains("player-1-active")){
+        button1.innerText = "✰"
+    } else {button1.innerText = "☁"}
+})
+
+button2.addEventListener("click", function(event){
+    if(promptTurn.classList.contains("player-1-active")){
+        button2.innerText = "✰"
+    } else {button2.innerText = "☁"}
+})
+
+button3.addEventListener("click", function(event){
+    if(promptTurn.classList.contains("player-1-active")){
+        button3.innerText = "✰"
+    } else {button3.innerText = "☁"}
+})
+
+button4.addEventListener("click", function(event){
+    if(promptTurn.classList.contains("player-1-active")){
+    button4.innerText = "✰"
+} else {button4.innerText = "☁"}
+})
+
+button5.addEventListener("click", function(event){
+    if(promptTurn.classList.contains("player-1-active")){
+        button5.innerText = "✰"
+    } else {button5.innerText = "☁"}
+    
+})
+button6.addEventListener("click", function(event){
+    if(promptTurn.classList.contains("player-1-active")){
+        button6.innerText = "✰"
+    } else {button6.innerText = "☁"} 
+})
+button7.addEventListener("click", function(event){
+    if(promptTurn.classList.contains("player-1-active")){
+        button7.innerText = "✰"
+    } else {button7.innerText = "☁"} 
+})
+button8.addEventListener("click", function(event){
+    if(promptTurn.classList.contains("player-1-active")){
+        button8.innerText = "✰"
+    } else {button8.innerText = "☁"} 
+})
+button9.addEventListener("click", function(event){
+    if(promptTurn.classList.contains("player-1-active")){
+        button9.innerText = "✰"
+    } else {button9.innerText = "☁"} 
+})
+function switchPlayer(){
+    if (promptTurn.classList.contains("player-1-active")){
+        promptTurn.classList.add("player-2-active")
+        promptTurn.classList.remove("player-1-active")
+    } else if (promptTurn.classList.contains("player-2-active")){
+        promptTurn.classList.add("player-1-active")
+        promptTurn.classList.remove("player-2-active")
+    }
+}
+function displayPlayerTurn(){
+    if(promptTurn.classList.contains("player-1-active")){
+        displaySymbol.innerText = "✰"
+    } else {
+        displaySymbol.innerText = "☁"
+    }
+}
 
 // Function(s) to display the game board and user data:
 
