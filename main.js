75d557988c6238f77cfc10dc7536a286ae051052
Jen Ziel <jenziel400@gmail.com>
2023-06-09 17:35:04 -0400

    Add gameplay logic to update the data model.

--- main.js
+++ main.js
@@ -6,97 +6,134 @@ var displayWhoseTurn = document.querySelector(".display-symbol")
 var buttonContainer = document.querySelectorAll(".grid-container")
 var cellButtons = document.querySelectorAll(".cell-button")
 var mainPane = document.querySelector("main")
-var button1 = document.querySelector("#btn1")
-var button2 = document.querySelector("#btn2")
-var button3 = document.querySelector("#btn3")
-var button4 = document.querySelector("#btn4")
-var button5 = document.querySelector("#btn5")
-var button6 = document.querySelector("#btn6")
-var button7 = document.querySelector("#btn7")
-var button8 = document.querySelector("#btn8")
-var button9 = document.querySelector("#btn9")
+var btn1 = document.querySelector("#btn1")
+var btn2 = document.querySelector("#btn2")
+var btn3 = document.querySelector("#btn3")
+var btn4 = document.querySelector("#btn4")
+var btn5 = document.querySelector("#btn5")
+var btn6 = document.querySelector("#btn6")
+var btn7 = document.querySelector("#btn7")
+var btn8 = document.querySelector("#btn8")
+var btn9 = document.querySelector("#btn9")
 
-var buttonsArray = [button1, button2, button3, button4, button5, button6, button7, button8, button9]
+// var ButtonsArray  = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9]
+var buttonsObjectArray = [
+    {name: "btn1", hasBeenClicked: false,}, {name: "btn2", hasBeenClicked: false}, {name: "btn3", hasBeenClicked: false}, {name: "btn4", hasBeenClicked: false},
+    {name: "btn5", hasBeenClicked: false}, {name: "btn6", hasBeenClicked: false}, {name: "btn7", hasBeenClicked: false}, {name: "btn8", hasBeenClicked: false},
+    {name: "btn9", hasBeenClicked: false}]
+
 var allTiles = ["btn1", "btn2", "btn3", "btn4", "btn5", "btn6", "btn7", "btn8", "btn9"]
-var winningCombinations = [["btn1", "btn2", "btn3"], ["btn4", "btn5", "btn6"], ["btn7", "btn8", "btn9"], ["btn 1", "btn4", "btn7"], ["btn2", "btn5", "btn8"], ["btn3", "btn6", "btn9"], ["btn1", "btn5", "btn9"], ["btn3", "btn5", "btn7"]]
+var winningCombinations = [["btn1", "btn2", "btn3"], ["btn4", "btn5", "btn6"], ["btn7", "btn8", "btn9"], ["btn1", "btn4", "btn7"], ["btn2", "btn5", "btn8"], ["btn3", "btn6", "btn9"], ["btn1", "btn5", "btn9"], ["btn3", "btn5", "btn7"]]
 
 var players = [ {
     name: "✰",
-    tilesSelected: [],
+    history: [],
     isTheirTurn: true,
     id: "player1",
+    isWinner: false,
+    isDraw: false,
 }, {
     name: "☁",
-    tilesSelected: [],
+    history: [],
     isTheirTurn: false,
     id: "player2",
+    isWinner: false,
+    isDraw: false, 
 }]
+
 // Event listeners:
 
-mainPane.addEventListener("click", function(){
-    reAssignWhoseTurn()
-    switchHeading()
-    displayPlayerTurn()
+mainPane.addEventListener("click", function(event){
+    var button = event.target.closest(".cell-button")
+    for( var i = 0; i < buttonsObjectArray.length; i++){
+        if (button.id === buttonsObjectArray[i].name){
+            buttonsObjectArray[i].hasBeenClicked = true
+            displayPlayerTurn()
+            updateGridSymbol(button)
+            addButtonToHistory(button.id)
+            checkForWinner()
+            reAssignWhoseTurn()
+        } else {
+        letPlayerRepeatTurn()
+        displayPlayerTurn()
+        checkForWinner()
+    }
+}  
 })
 
-button1.addEventListener("click", function(event){
-    updateGridSymbol(button1)
-    addButtonToHistory("btn1")
-})
+// button1.addEventListener("click", function(event){
+//     updateGridSymbol(button1)
+//     addButtonToHistory("btn1")
+//     button1.hasBeenClicked = true
+// })
 
-button2.addEventListener("click", function(event){
-    updateGridSymbol(button2)
-    addButtonToHistory("btn2")
-    })
+// button2.addEventListener("click", function(event){
+//     updateGridSymbol(button2)
+//     addButtonToHistory("btn2")
+//     button2.hasBeenClicked = true
+//     })
 
-button3.addEventListener("click", function(event){
-    updateGridSymbol(button3)
-    addButtonToHistory("btn3")
-    })
+// button3.addEventListener("click", function(event){
+//     updateGridSymbol(button3)
+//     addButtonToHistory("btn3")
+//     button3.hasBeenClicked = true
+//     })
 
-button4.addEventListener("click", function(event){
-    updateGridSymbol(button4)
-    addButtonToHistory("btn4")
-    })
+// button4.addEventListener("click", function(event){
+//     updateGridSymbol(button4)
+//     addButtonToHistory("btn4")
+//     button4.hasBeenClicked = true
+//     })
 
-button5.addEventListener("click", function(event){
-    updateGridSymbol(button5)
-    addButtonToHistory("btn5")
-    })
+// button5.addEventListener("click", function(event){
+//     updateGridSymbol(button5)
+//     addButtonToHistory("btn5")
+//     button5.hasBeenClicked = true
+//     })
 
-button6.addEventListener("click", function(event){
-    updateGridSymbol(button6)
-    addButtonToHistory("btn6")
-    })
+// button6.addEventListener("click", function(event){
+//     updateGridSymbol(button6)
+//     addButtonToHistory("btn6")
+//     button6.hasBeenClicked = true
+//     })
 
-button7.addEventListener("click", function(event){
-    updateGridSymbol(button7)
-    addButtonToHistory("btn7")
-    })
+// button7.addEventListener("click", function(event){
+//     updateGridSymbol(button7)
+//     addButtonToHistory("btn7")
+//     button7.hasBeenClicked = true
+//     })
 
-button8.addEventListener("click", function(event){
-    updateGridSymbol(button8)
-    addButtonToHistory("btn8")
-    })
+// button8.addEventListener("click", function(event){
+//     updateGridSymbol(button8)
+//     addButtonToHistory("btn8")
+//     button8.hasBeenClicked = true
+//     })
 
-button9.addEventListener("click", function(event){
-    updateGridSymbol(button9)
-    addButtonToHistory("btn9")
-    })
+// button9.addEventListener("click", function(event){
+//     updateGridSymbol(button9)
+//     addButtonToHistory("btn9")
+//     button9.hasBeenClicked = true
+//     })
 
 function updateGridSymbol(buttonName){
-        if(players[0].isTheirTurn === true){
-           buttonName.innerText = [players[0].name]
-        } else {buttonName.innerText = [players[1].name]}
+        if (buttonName.innerText === ""){
+            if(players[0].isTheirTurn === true){
+                return buttonName.innerText = players[0].name
+             } else {return buttonName.innerText = players[1].name}
+        }
+        if (buttonName.innerText.length > 0){
+            letPlayerRepeatTurn()
+            keepHeadingTheSame()
+        }
     }
 
 
 function addButtonToHistory(specificButton){
     var buttonName = specificButton
     if (players[0].isTheirTurn === true){
-        players[0].tilesSelected.push(buttonName)
+        players[0].history.push(buttonName)
     } else {
-        players[1].tilesSelected.push(buttonName)
+        players[1].history.push(buttonName)
     }
 }
 
@@ -111,24 +148,38 @@ function reAssignWhoseTurn(){
     }
 }
 
-function switchHeading(){
-    if (players[0].isTheirTurn === true){
-        promptTurn.classList.add(`${players[0].id}-active`)
-        promptTurn.classList.remove(`${players[1].id}-active`)
+function letPlayerRepeatTurn(){
+    if(players[0].isTheirTurn === true){
+        players[1].isTheirTurn = false
+        players[0].isTheirTurn = true
     } else {
-        promptTurn.classList.add(`${players[1].id}-active`)
-        promptTurn.classList.remove(`${players[0].id}-active`)
-
+        players[1].isTheirTurn = true
+        players[0].isTheirTurn = false
     }
 }
 
 function displayPlayerTurn(){
     if(players[0].isTheirTurn === true){
-        displayWhoseTurn.innerText = [players[0].name]
+        displayWhoseTurn.innerText = players[0].name
     } else {
-        displayWhoseTurn.innerText = [players[1].name]
+        displayWhoseTurn.innerText = players[1].name
     }
 }
 function checkForWinner(){
-    if players[0].history.includes(["btn1", "btn2", "btn3"],)
+    var p1Moves = players[0].history
+    var p2Moves = players[1].history
+    for (var i = 0; i < winningCombinations.length; i++){
+        if (p1Moves.includes(winningCombinations[i][0]) && p1Moves.includes(winningCombinations[i][1]) && p1Moves.includes(winningCombinations[i][2])){
+            return players[0].isWinner = true
+        }
+        if (p2Moves.includes(winningCombinations[i][0]) && p2Moves.includes(winningCombinations[i][1]) && p2Moves.includes(winningCombinations[i][2])){
+            return players[1].isWinner = true
+        }
+    } 
+    if (p1Moves.length === 5 || p2Moves.length === 5){
+        players[0].isDraw = true
+        players[1].isDraw = true  
+    }
 }
+
+
